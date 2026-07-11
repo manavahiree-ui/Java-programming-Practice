f0d6b5fd96be3ac84e3fa90af2d782c8a9374698
manavahiree-ui <manavahiree@gmail.com>
2026-07-11 17:40:06 +0530

    Day 2 Java programs

---
+++ BufferedIO.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class BufferedIO
+{
+    public static void main(String A[])
+    {
+        BufferedReader bobj = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter your Name:");
+        String name = bobj.readLine();        //checked Exception
+
+        System.out.println("Hello"+name);
+    }
+}
